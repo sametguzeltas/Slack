d50ffd1821a9cb0ab879518b10cba91af45cc40f
samedguzeltas <samedguzeltas@gmail.com>
2020-05-12 17:22:41 +0100

    add Login.java

---
+++ src/day_1/Login.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("AC: User should be able to login with valid credentials");
+
+
+    }
+}
