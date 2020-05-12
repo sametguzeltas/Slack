8f99e9daf4950a5c122c57498499373f9aa1a80d
samedguzeltas <62351808+samedguzeltas@users.noreply.github.com>
2020-05-12 17:23:46 +0100

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -5,6 +5,10 @@ public class Login {
     public static void main(String[] args) {
 
         System.out.println("AC: User should be able to login with valid credentials");
+        
+        //Slack users can login succesfully on gitHub
+        
+        // brand new changes
 
 
     }
