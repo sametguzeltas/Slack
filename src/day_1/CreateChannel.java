2ee1cebde4e24606b7d0c4986028545c6fa0f289
samedguzeltas <samedguzeltas@gmail.com>
2020-05-12 16:54:48 +0100

    add CreateChannel.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class CreateChannel {
+
+    public static void main(String[] args) {
+
+        System.out.println("Acceptance criteria: User should be able to create channels");
+
+    }
+}
