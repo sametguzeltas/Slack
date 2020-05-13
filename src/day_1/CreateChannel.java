42e7d6050216a6274d6d3df1b4adb6f549642d16
samedguzeltas <62351808+samedguzeltas@users.noreply.github.com>
2020-05-13 15:59:42 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -1,10 +1,15 @@
 package day_1;
 
 public class CreateChannel {
-
     public static void main(String[] args) {
 
         System.out.println("Acceptance criteria: User should be able to create channels");
 
+        String s= "a";
+        if(s.contains("a")){
+        System.out.println("contains");
+        }else{
+            System.out.println("not");
+}
     }
 }
