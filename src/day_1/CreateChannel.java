fe43c8fcea0ee18ebfd06c6cee059f0c8e0a060d
samedguzeltas <samedguzeltas@gmail.com>
2020-05-16 20:22:18 +0100

    add Emoji.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,7 +4,6 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("Acceptance criteria: User should be able to create channels");
-
         int x=0;  //from intellij
         if (x==0){
             System.out.println("equal");
---
+++ src/day_1/Emoji.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Emoji {
+
+    public static void main(String[] args) {
+
+        System.out.println("User can send emoji ");
+    }
+}
