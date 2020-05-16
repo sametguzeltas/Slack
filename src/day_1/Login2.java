4960b2e6e3cd8f51238bb7284d5647ae1183ec47
samedguzeltas <samedguzeltas@gmail.com>
2020-05-16 18:36:49 +0100

    add Login2.java

---
+++ src/day_1/Login2.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Login2 {
+
+    public static void main(String[] args) {
+
+        //Slack users can login succesfully
+
+        //Brand new changes from GitHub
+
+
+    }
+}
